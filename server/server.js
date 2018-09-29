3c545fe7c64db3278d19ea81278eddd8b1b9e782
Bhupender Singh <bhupenders225@gmail.com>
2018-09-29 20:55:17 +0530

    seted up front-end and backend socket

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
-const express = require('express'),
-    app = express(),
-    path = require('path');
+const
+    express  = require('express'),
+    app      = express(),
+    http     = require('http'),
+    socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port, ()=> {
+io.on('connection', (socket) => {
+    console.log('Connection made successfully.');
+});
+
+server.listen(port, () => {
     console.log(`Server is running in port: ${3000}`);
 });
